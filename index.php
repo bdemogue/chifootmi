9fbc29a9e5d778b874fa053a10008b2d1b827189
Demogue <bbdishere@gmail.com>
2018-02-13 09:14:12 +0100

    Title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Chifoumi</title>
+        <title>Chifootmi</title>
         <link rel="stylesheet" type="text/css" href="<?php echo $hmr ?>assets/styles/styles.css">   
     </head>
     <body>
